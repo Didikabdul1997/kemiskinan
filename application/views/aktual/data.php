8918eaf5039f3772ddefc5cf5acd1aaaf03ae445
Lenovo <40739495+Didikabdul1997@users.noreply.github.com>
2020-09-05 19:16:23 +0700

    Update data.php

--- application/views/aktual/data.php
+++ application/views/aktual/data.php
@@ -111,7 +111,7 @@
         <form action="<?= base_url(); ?>aktual/simpan" method="post" enctype="multipart/form-data">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Input Data Kemiskinan</h5>
+                    <h5 class="modal-title" id="exampleModalLongTitle">Edit Data Kemiskinan</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
